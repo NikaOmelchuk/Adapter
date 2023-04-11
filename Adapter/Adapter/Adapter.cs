4b9c4a0451b189028511193f6fc0ceefeb40f1b5
NikaOmelchuk <120196531+NikaOmelchuk@users.noreply.github.com>
2023-04-11 05:39:11 +0300

    Add files via upload

---
+++ Adapter/Adapter/Adapter.cs
@@ -0,0 +1,154 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace Adapter
+{
+    public interface DataGrid
+    {
+        void SaveTo(string fileName);
+    }
+
+    public class SaveToBinaryAdapter : DataGrid
+    {
+        private DataGridView dg;
+
+        public SaveToBinaryAdapter(DataGridView data)
+        {
+            dg = data;
+        }
+
+        public void SaveTo(string fileName)
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.OpenOrCreate)))
+            {
+                for (int i = 0; i < dg.RowCount; i++)
+                {
+                    for (int l = 0; l < dg.ColumnCount; l++)
+                    {
+                        if (dg[l, i].Value != null)
+                        {
+                            writer.Write(dg[l, i].Value.ToString() + "\t");
+                        }
+                    }
+                    writer.Write("\n");
+                }
+            }
+        }
+    }
+
+    public class SaveToTextAdapter : DataGrid
+    {
+        private DataGridView dg;
+
+        public SaveToTextAdapter(DataGridView data)
+        {
+            dg = data;
+        }
+
+        public void SaveTo(string fileName)
+        {
+            StreamWriter writer = new StreamWriter(fileName);
+
+            for (int i = 0; i < dg.RowCount; i++)
+            {
+                for (int l = 0; l < dg.ColumnCount; l++)
+                {
+                    if (dg[l, i].Value != null)
+                    {
+                        writer.Write(dg[l, i].Value.ToString() + "\t");
+                    }
+                }
+                writer.WriteLine();
+            }
+            writer.Close();
+        }
+    }
+
+    public interface Filee
+    {
+        void SaveToGrid(string fileName, DataGridView dg);
+    }
+
+    public class BinaryAdapter : Filee
+    {
+       public void SaveToGrid(string fileName, DataGridView dg)
+       {
+            using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            {
+                string line;
+                string zap = "";
+
+                int r = 0;
+                int c = 0;
+
+                while (reader.PeekChar() > -1)
+                {
+                    dg.Rows.Add();
+                    line = reader.ReadString();
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (line[i].ToString() == "\n")
+                        {
+                            r++;
+                            c = 0;
+                        }
+                        else if (line[i].ToString() != "\t")
+                        {
+                            zap += line[i];
+                        }
+                        else
+                        {
+                            dg[c, r].Value = zap;
+                            c++;
+                            zap = "";
+                        }
+                    }
+                }
+                for (int i = dg.RowCount-2; i > 0; i--)
+                {
+                    if (dg[0, i].Value == null && dg[1, i].Value == null
+                        && dg[2, i].Value == null && dg[3, i].Value == null
+                        && dg[4, i].Value == null)
+                        dg.Rows.RemoveAt(i);
+                }
+            }
+        }
+    }
+
+    public class TextAdapter : Filee
+    {
+        public void SaveToGrid(string fileName, DataGridView dg)
+        {
+            string line;
+            string zap = "";
+
+            int r = 0;
+            int c = 0;
+
+            StreamReader sr = new StreamReader(fileName);
+            line = sr.ReadLine();
+
+            while (line != null)
+            {
+                dg.Rows.Add();
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if(line[i].ToString() !="\t")
+                    {
+                        zap += line[i];
+                    }
+                    else 
+                    {
+                        dg[c, r].Value = zap;
+                       c++;
+                        zap = "";
+                    }
+                }
+                line = sr.ReadLine();
+                r++;
+                c = 0;
+            }
+            sr.Close();
+        }
+    }
+}
---
+++ Adapter/Adapter/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adapter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveToBinaryAdapter adapter = new SaveToBinaryAdapter(dataGridView1);
+            adapter.SaveTo("data.bin");
+            MessageBox.Show("Сохранено!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BinaryAdapter adapter = new BinaryAdapter();
+            adapter.SaveToGrid("data.bin", dataGridView1);
+        }
+
+        void hh()
+        {
+            if (dataGridView1.RowCount > 0 && dataGridView1[0, 0].Value != null)
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button5.Enabled = true;
+            }
+            else
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button5.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hh();
+        }
+
+        private void dataGridView1_CurrentCellChanged(object sender, EventArgs e)
+        {
+            hh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveToTextAdapter adapter = new SaveToTextAdapter(dataGridView1);
+            adapter.SaveTo("data.txt");
+            MessageBox.Show("Сохранено!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            TextAdapter adapter = new TextAdapter();
+            adapter.SaveToGrid("data.txt", dataGridView1);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
